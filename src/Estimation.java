4a3ce11a04aa7024a90025e916cf61ffaf2c72ff
Olexiy Koval <kalex32@ukr.net>
2018-08-26 02:04:17 +0300

    Вывести на печать в соответсвии с заданием

--- src/Estimation.java
+++ src/Estimation.java
@@ -2,22 +2,21 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 class Estimation implements ListOfEstimation {
-    private int estimation;
 
     @Override
-    public void receiptOfEstimation(Student[] students) {
+    public void receiptOfEstimation(Student[] students ) {
         Scanner scanner = new Scanner(System.in);
-        for (int i = 0; i < students.length; i++) {
-            System.out.println("Введите оценку 1 для студента: " + students[i].getLastName() +", "+
-                    students[i].getYearOfBirth() + " года рождения");
-            students[i].setEst1(scanner.nextInt());
-            System.out.println("Введите оценку 2 для студента: " + students[i].getLastName() +", "+
-                    students[i].getYearOfBirth() + " года рождения");
-            students[i].setEst2(scanner.nextInt());
-            System.out.println("Введите оценку 3 для студента: " + students[i].getLastName() +", "+
-                    students[i].getYearOfBirth() + " года рождения");
-            students[i].setEst3(scanner.nextInt());
-            students[i].setAverageEst(setAverageEst(students[i].getEst1(), students[i].getEst2(), students[i].getEst3()));
+        for (Student i : students) {
+            System.out.println("Введите оценку 1 для студента: " + i.getLastName() +", "+
+                    i.getYearOfBirth() + " года рождения");
+            i.setEst1(scanner.nextInt());
+            System.out.println("Введите оценку 2 для студента: " + i.getLastName() +", "+
+                    i.getYearOfBirth() + " года рождения");
+            i.setEst2(scanner.nextInt());
+            System.out.println("Введите оценку 3 для студента: " + i.getLastName() +", "+
+                    i.getYearOfBirth() + " года рождения");
+            i.setEst3(scanner.nextInt());
+            i.setAverageEst(setAverageEst(i.getEst1(), i.getEst2(), i.getEst3()));
 
         }
         System.out.println(Arrays.toString(students));
--- src/RunStudentsAndEstimate.java
+++ src/RunStudentsAndEstimate.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class RunStudentsAndEstimate {
     public static void main(String[] args) {
         Student[] students = new Student[3];
-        students[5]=new Student("Кандиба", 2000);
-        students[6]=new Student("Куско", 1998);
-        students[7]=new Student("Примакин", 1999);
+        students[0]=new Student("Кандиба", 2000);
+        students[1]=new Student("Куско", 1998);
+        students[2]=new Student("Примакин", 1999);
 
 
         System.out.println(Arrays.toString(students));
