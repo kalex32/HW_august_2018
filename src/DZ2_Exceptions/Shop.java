5d38272983389faa16c9232ad3c803a315a58188
Olexiy Koval <kalex32@ukr.net>
2018-08-29 15:45:35 +0300

    Прописать методы

--- src/DZ2_Exceptions/Shop.java
+++ src/DZ2_Exceptions/Shop.java
@@ -1,11 +1,51 @@
 package DZ2_Exceptions;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 class Shop {
 
-    void toChoose(){
-        System.out.println("Выберите пункт меню:"+"\n"+
-                "1. Клиент"+"\n"+
-                "2. Администратор");
+    void toChoose() {
+        for (; ; ) {
+            System.out.println("Выберите пункт меню:" + "\n" +
+                    "1. Клиент" + "\n" +
+                    "2. Администратор" + "\n" +
+                    "3. Выход");
 
+            Scanner scanner = new Scanner(System.in);
+            try {
+                switch (scanner.nextInt()) {
+                    case 1:
+                        System.out.println("Выберите пункт меню:" + "\n" +
+                                "1. Сделать заказ" + "\n" +
+                                "2. Оплатить" + "\n" +
+                                "3. Выход");
+                        switch (scanner.nextInt()) {
+                            case 1:
+
+                            case 2:
+
+                            case 3:
+                        }
+                    case 2:
+                        System.out.println("Выберите пункт меню:" + "\n" +
+                                "1. Добавить товар в магазин" + "\n" +
+                                "2. Зарегистрировать продажу" + "\n" +
+                                "3. Добавить покупателя в черный список" + "\n" +
+                                "4. Выход");
+                        switch (scanner.nextInt()) {
+                            case 1:
+
+                            case 2:
+
+                            case 3:
+
+                            case 4:
+                        }
+                }
+            } catch (InputMismatchException i) {
+                toChoose();
+            }
+        }
     }
 }
