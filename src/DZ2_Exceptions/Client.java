94c7eecfab294244a69a5bd978ea90037361b093
Olexiy Koval <kalex32@ukr.net>
2018-09-01 01:01:32 +0300

    Метод размещения заказа козлит 2

--- src/DZ2_Exceptions/Client.java
+++ src/DZ2_Exceptions/Client.java
@@ -3,14 +3,14 @@ package DZ2_Exceptions;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-class Client implements Clients{
+class Client implements Clients {
     private String nameClient;
 
     {
         System.out.println("Введите имя");
     }
 
-   private Scanner scannerClient = new Scanner(System.in);
+    private Scanner scannerClient = new Scanner(System.in);
 
     Client() {
         this.nameClient = scannerClient.nextLine();
@@ -25,23 +25,26 @@ class Client implements Clients{
 
     @Override
     public void toOrder() {
-        for (; ; ) {
-            System.out.println("Выберите, пожалуйста, товар:");
-            for (Product p : administrator.products) System.out.println(p.toString());
-            System.out.println();
-            cart.add(administrator.products.get(scannerClient.nextInt() - 1));
-            System.out.println("Ваша корзина:");
-            System.out.println();
-            for (Product p : cart) System.out.println(p.toString());
-            System.out.println("Хотите добавить еще товар?:" + "\n" +
-                    "1. Добавить" + "\n" +
-                    "2. Нет");
-            if (scannerClient.nextInt() != 1) break;
+        try {
+            for (; ; ) {
+                System.out.println("Выберите, пожалуйста, товар:");
+                for (Product p : administrator.products) System.out.println(p.toString());
+                System.out.println();
+                cart.add(administrator.products.get(scannerClient.nextInt() - 1));
+                System.out.println("Ваша корзина:");
+                System.out.println();
+                for (Product p : cart) System.out.println(p.toString());
+                System.out.println("Хотите добавить еще товар?:" + "\n" +
+                        "1. Добавить" + "\n" +
+                        "2. Нет");
+                if (scannerClient.nextInt() != 1) break;
+            }
+        } catch (IndexOutOfBoundsException i){
+            toOrder();
         }
     }
+        @Override
+        public void toPay () {
 
-    @Override
-    public void toPay() {
-
+        }
     }
-}
